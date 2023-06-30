39048c416377386bcc85a896d663b0fa73e61d48
JoseBaldor <jfco37237@gmail.com>
2023-06-30 07:31:57 -0600

    Class 03

---
+++ 03 C/homework03.cpp
@@ -0,0 +1,58 @@
+// Do not change any of the function names
+
+function mayuscula(nombre) {
+    //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
+    //ej: Recibe "mario" ----> Devuelve "Mario"
+    //Tu código:
+
+}
+
+
+function StringInvertida(str) {
+    // La funcion recibe un String que devuelva la misma cadena de caracteres, pero invertida.
+    // Ej: 'toni' debe regresar ---> 'inot' 
+    //Tu código:
+
+}
+
+
+function numberOfCharacters(string, dato) {
+    //La función recibe un string y un dato. Recorre el srting y devuelve el caracter(dato) con el número de veces que aparece 
+    //en caso de no encontrarlo devuele 0
+    //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" "a"|| Devuelve ---> {5}  ya que exsiten 5 "a" en el string
+    //Escribe tu código aquí
+
+}
+
+
+
+function capToFront(s) {
+    //Realiza una función que reciba como parámetro un string y mueva todas las letras mayúsculas
+    //al principio de la palabra.
+    //Ejemplo: soySUPERMAN -> SUPERMANsoy
+    //Escribe tu código aquí
+
+}
+
+
+function asAmirror(str) {
+    //La función recibe una frase. 
+    //Escribe una función que tome la frase recibida y la devuelva de modo tal que se pueda leer de izquierda a derecha 
+    //pero con cada una de sus palabras invertidas, como si fuera un espejo.
+    //Ej: Recibe ---> "The Class is very Easy!" || Devuelve ---> "!ehT ssalC si yrev ysaE"
+    //Escribe tu código aquí
+
+}
+
+
+function deleteAbc(cadena) {
+    //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
+    //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
+    //ej. "Estamos por terminar la primer semana de clase" ----> debe regresar "Estmos por terminr l primer semn de lse"
+    //Escribe tu código aquí
+    //Hola
+
+}
+
+
+
